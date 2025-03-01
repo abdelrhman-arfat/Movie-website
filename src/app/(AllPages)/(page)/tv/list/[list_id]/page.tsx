184910d1c72f7ea0a3e3daa2c8b33788bc05a309
boody <abdoarfat2006@gmail.com>
2025-03-01 22:05:10 +0200

    build time issue

--- src/app/(AllPages)/(page)/tv/list/[list_id]/page.tsx
+++ src/app/(AllPages)/(page)/tv/list/[list_id]/page.tsx
@@ -1,10 +1,8 @@
 import TvListSection from "@/app/components/tv/TvListSection";
 import React from "react";
 
-
-const Page = async ({ params }: { params: { list_id: string } }) => {
-
-  const { list_id } = await params;
+const Page = ({ params }: { params: { list_id: string } }) => {
+  const { list_id } = params;
   return (
     <div className="w-full bg-neutral-800 py-6">
       <div className="mx-auto w-[90%]">
